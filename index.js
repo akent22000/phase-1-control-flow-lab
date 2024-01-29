5a70845c03d764182efa64fa4af72654bb6e66bd
amandalocher <akent22000@gmail.com>
2024-01-29 17:15:09 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+    let result;
+    if (ride <= 400) {
+      result = 'This one is on me!';
+
+    } else if (ride >=400 && ride <= 2000) {
+      result = 'That will be twenty bucks.';
+
+    } else if (ride >=2000 && ride <= 2500) {
+      result = 'I will gladly take your thirty bucks.';
+      
+    } else if (ride >=2500) {
+      result = 'No can do.';
+      
+    } else {
+      result = 'No can do.';
+    }
+    return result;
+  }
+
+
+function ternaryCheckCity(city){
+  // Write your code here!
+
+    if (city === "NYC") {
+        return "Ok, sounds good.";
+    }
+    else{
+        return "No go.";
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+    if (tip === "generous") {
+        return "Thank you so much.";
+    } else if (tip === "not as generous") {
+        return "Thank you.";
+    } else {
+        return "Bye.";
+    }
+
 }
